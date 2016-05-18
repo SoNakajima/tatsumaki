fa556de05a2b1a04fc3ed873c35424282bd6d57e
SoNakajima <Ararara@net92-dhcp-199.sfc.keio.ac.jp>
2016-05-18 15:09:08 +0900

    uzumaki
    うずまき軽く修正

--- src/solver.cpp
+++ src/solver.cpp
@@ -10,16 +10,17 @@
 #include "solver.h"
 
 void Solver::calcUpdate(){
-    int n = 32; // 32 個の四角で1つのうずを書く
-    int m = 9;  // 3 回巻く
+    int n = 100; // 32 個の四角で1つのうずを書く
+    int m = 3;  // 3 回巻く
     
     for(int h = 0; h < m; h = h + 1) {
         for(int i = 0; i < n; i++) {
             
             float theta = i * (2 * 3.14 / n);
-            float r = 80 / (m * n) * i + 80 / m * h;
-            particle_position[i].x  = cos(theta)*r + 200;
-            particle_position[i].y =  sin(theta)*r + 200;
+            //float r = 120 / (m * n) * i + 120 / m * h;
+            float r = 30;
+            particle_position[i].x  = cos(theta)*r*theta + 200;
+            particle_position[i].y =  sin(theta)*r*theta + 200;
         }
     }
     
